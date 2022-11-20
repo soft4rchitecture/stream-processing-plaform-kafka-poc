fc3326647239fad17691990113d972e80556c3db
duuude <architect@soft4rchitecture.org>
2022-11-20 23:15:18 +0100

    fix (infrastructure): replace shell based bootstrap implementation (WIP)
    kafka topics creation should happen in the  managed environnement of a kafka container because the scripts actually needs the artifacts created during kafka build to run, at a given position in the filesystem. Such a constraint forces to do a lot of work for this to work in a portable way using shell scripts. The better solution would be to spin up an other kafka container that's behaving as the client to request le kafka cluster

--- scripts/kafka-topics-create-and-list.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
- ./scripts/bin/kafka-topics.sh --create --bootstrap-server localhost:29092 --replication-factor 1 --partitions 1 --topic integer
- ./scripts/bin/kafka-topics.sh --create --bootstrap-server localhost:29092 --replication-factor 1 --partitions 1 --topic odd
- ./scripts/bin/kafka-topics.sh --create --bootstrap-server localhost:29092 --replication-factor 1 --partitions 1 --topic even
-
-  ./scripts/bin/kafka-topics.sh --list --bootstrap-server localhost:29092
